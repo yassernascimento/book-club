ee2d123e5ace8781aaaf0fe76c627a3b94fc2efb
Yasser Nascimento <yassern@protonmail.com>
2021-03-27 11:39:40 -0300

    setup sequelize

---
+++ apps/api/sequelize.config.js
@@ -0,0 +1,15 @@
+console.log({
+  username: process.env,
+  foo: process.env.DB_DATABASE,
+});
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+};
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const {
+  DB_USERNAME: username,
+  DB_PASSWORD: password,
+  DB_DATABASE: database,
+  DB_HOST: host,
+  DB_PORT: port,
+} = process.env;
+
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host,
+      port: +port,
+      username,
+      password,
+      database,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
